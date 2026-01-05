545d391c235556500f1a9afb6d961b4d8327dfea
jburton321 <jburton.mail10@yahoo.com>
2026-01-05 17:27:43 -0500

    Updated tailwind.css

--- src/GiveawayDesktop/GiveawayDesktop.tsx
+++ src/GiveawayDesktop/GiveawayDesktop.tsx
@@ -40,9 +40,10 @@ export const GiveawayDesktop = ({
       <div className="px-4 lg:px-12 flex flex-col gap-0 items-start justify-start self-stretch shrink-0 relative">
         <Navbar />
         <div className="flex flex-col items-center justify-start self-stretch shrink-0 relative">
-          <div className="shrink-0 w-[300px] h-[159.14px] relative overflow-hidden flex items-center justify-center z-50">
+          <div className="shrink-0 w-[300px] h-[159.14px] relative flex items-center justify-center z-50">
+            <div className="absolute bottom-0 w-[280px] h-[100px] bg-white rounded-t-full"></div>
             <img
-              className="w-full h-auto"
+              className="w-full h-auto relative z-10"
               src="sticker.png"
             />
           </div>
